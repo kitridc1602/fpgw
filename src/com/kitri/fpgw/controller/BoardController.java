f0f7983933422c75d8c1cb0cc51b3d3582b7cb07
mia <evamiya1@gmail.com>
2016-03-11 10:00:59 +0900

    게시판 commit

--- src/com/kitri/fpgw/controller/BoardController.java
+++ src/com/kitri/fpgw/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.kitri.fpgw.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,207 +17,255 @@ import com.kitri.fpgw.model.BoardCommentDto;
 import com.kitri.fpgw.model.BoardMainDto;
 import com.kitri.fpgw.service.BoardService;
 
-
 @Controller
-@RequestMapping(value="/board")
+@RequestMapping(value = "/board")
 public class BoardController {
-	
+
 	@Autowired
 	private BoardService boardService;
 
-	
-	//寃뚯떆湲� 由ъ뒪�듃
-	@RequestMapping(value="basicboardList.html")
-	public ModelAndView basicboardList(String strGroup, String strKind ,String workkind, String subworkkind) throws Exception{
-		
+	// 기본 게시판 리스트
+	@RequestMapping(value = "basicboardList.html")
+	public ModelAndView basicboardList(String strGroup, String strKind, String workkind, String subworkkind)
+			throws Exception {
+
 		ModelAndView mav = new ModelAndView();
-		
+
 		BoardMainDto boardMainDto = new BoardMainDto();
 		boardMainDto.setStrGroup(strGroup);
 		boardMainDto.setStrKind(strKind);
-				
-		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
-		
-		//硫붿씤由ъ뒪�듃
-		mav.addObject("list", list);
-		
-		//�긽�떒 移댄뀒怨좊━猷⑦듃 �옄�룞蹂�寃�    洹몃９ 移댁씤�듃 肄붾뱶濡� �굹以묒뿉 諛붽씀湲�
-		if("001".equals(workkind)){
-			mav.addObject("workKind", "寃뚯떆�뙋 >> 怨듭��궗�빆");
-		} else if("002".equals(workkind)) {
-			mav.addObject("workKind", "寃뚯떆�뙋 >> 遺��꽌蹂꾧쾶�떆�뙋");	
-		} else if("003".equals(workkind)) {
-			mav.addObject("workKind", "寃뚯떆�뙋 >> �옄�쑀寃뚯떆�뙋");	
-		} else if("004".equals(workkind)) {
-			mav.addObject("workKind", "寃뚯떆�뙋 >> KITRI STORY");	
-		}
-		//以묎컙遺�遺꾩옄�룞蹂�寃�
-		if("001".equals(subworkkind)){
-			mav.addObject("subworkkind","怨듭��궗�빆");
-		}else if("002".equals(subworkkind)){
-			mav.addObject("subworkkind","遺��꽌蹂꾧쾶�떆�뙋");
-		}else if("003".equals(subworkkind)){
-			mav.addObject("subworkkind","�옄�쑀寃뚯떆�뙋");
-		}else if("004".equals(subworkkind)){
-			mav.addObject("subworkkind","KITRI STORY");
-		}
-						
-		mav.setViewName("jsp/board/basicboardList");
-		
-		
-		return mav;
-		
-	}
-		
-	//湲��벐湲� 踰꾪듉 �늻瑜대㈃~~
-		@RequestMapping(value="/writego.html")
-		public String writeGo(){
 
-			return "jsp/board/basicBoardWrite"; 
+		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
+
+		mav.addObject("list", list);
+
+		if ("001".equals(workkind)) {
+			mav.addObject("workKind", "寃뚯떆�뙋 >> 怨듭��궗�빆");
+		} else if ("002".equals(workkind)) {
+			mav.addObject("workKind", "寃뚯떆�뙋 >> 遺��꽌蹂꾧쾶�떆�뙋");
+		} else if ("003".equals(workkind)) {
+			mav.addObject("workKind", "寃뚯떆�뙋 >> �옄�쑀寃뚯떆�뙋");
+		} else if ("004".equals(workkind)) {
+			mav.addObject("workKind", "寃뚯떆�뙋 >> KITRI STORY");
 		}
-		
-	//寃뚯떆�뙋 �씪諛섍��벐湲�
-	@RequestMapping(value="basicBoardWrite.html", method=RequestMethod.POST)
+
+		if ("001".equals(subworkkind)) {
+			mav.addObject("subworkkind", "怨듭��궗�빆");
+		} else if ("002".equals(subworkkind)) {
+			mav.addObject("subworkkind", "遺��꽌蹂꾧쾶�떆�뙋");
+		} else if ("003".equals(subworkkind)) {
+			mav.addObject("subworkkind", "�옄�쑀寃뚯떆�뙋");
+		} else if ("004".equals(subworkkind)) {
+			mav.addObject("subworkkind", "KITRI STORY");
+		}
+
+		mav.setViewName("jsp/board/basicboardList");
+
+		return mav;
+
+	}
+
+	// 기본 글쓰기 버튼 누를때
+	@RequestMapping(value = "/writego.html")
+	public String writeGo() {
+
+		return "jsp/board/basicBoardWrite";
+	}
+
+	// 기본 글 작성 후 입력 버튼 누르면
+	@RequestMapping(value = "basicBoardWrite.html", method = RequestMethod.POST)
 	public ModelAndView insert(BoardMainDto boardMainDto) throws Exception {
-				
+
 		boardService.addArticle(boardMainDto);
-		
-		ModelAndView mav= new ModelAndView();
-		
+
+		ModelAndView mav = new ModelAndView();
+
 		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
 		mav.addObject("list", list);
 		mav.setViewName("jsp/board/basicboardList");
-		
+
 		return mav;
-		
+
 	}
-	
-	//�떟湲�踰꾪듉 �겢由��떆 �씠�룞~~	
-	@RequestMapping (value="/rewritego.html")
-	public ModelAndView reWriteGo(@RequestParam(value="num") int intSeq ) throws Exception{
-		
-		ModelAndView mav =new ModelAndView();
+
+	// 답글 달기 버튼 누르면
+	@RequestMapping(value = "/rewritego.html")
+	public ModelAndView reWriteGo(@RequestParam(value = "num") int intSeq) throws Exception {
+
+		ModelAndView mav = new ModelAndView();
 		BoardMainDto boardMainDto = boardService.getArticle(intSeq);
 		mav.addObject("reply", boardMainDto);
 		mav.setViewName("jsp/board/replyWrite");
-		
+
 		return mav;
 	}
-	
-	//�떟湲��벐湲�
-	@RequestMapping(value="/rewrite.html")
+
+	// 기본 답글 쓰기
+	@RequestMapping(value = "/rewrite.html")
 	public ModelAndView replyInsert(BoardMainDto boardMainDto) throws Exception {
-		
+
 		boardService.addReply(boardMainDto);
 		ModelAndView mav = new ModelAndView();
-		
+
 		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
 		mav.addObject("list", list);
 		mav.setViewName("jsp/board/basicboardList");
-		
+
 		return mav;
 	}
-	
-	//�뙎湲��엯�젰
-	@RequestMapping(value="/addComment.html")
-	public @ResponseBody String addComment(BoardMainDto boardMainDto) throws Exception{
-		
-		//�엯�젰
-		
-		boardService.addComment(boardMainDto);
-		
-		JSONObject json = new JSONObject();
-		json.put("result", "sucess");
-		
-		return json.toJSONString();
+
+	// 글 수정 하기 버튼 누르면~
+	@RequestMapping(value = "/modifygo.html")
+	public ModelAndView modifyGo(@RequestParam(value = "num") int intSeq) throws Exception {
+
+		ModelAndView mav = new ModelAndView();
+
+		BoardMainDto boardMainDto = boardService.getArticle(intSeq);
+		mav.addObject("read", boardMainDto);
+		mav.setViewName("jsp/board/basicboardModify");
+
+		return mav;
 	}
-	
-	//�뙎湲� �궘�젣�븯湲�
-			@RequestMapping(value="/deletecomment.html")
-			public ModelAndView deleteComment(@RequestParam(value="num") int intSeq, BoardMainDto boardMainDto) throws Exception{
-				boardService.deleteArticle(intSeq);
-				
-				System.out.println(intSeq);
-				
-				ModelAndView mav= new ModelAndView();
-				ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
-				mav.addObject("list", list);
-				mav.setViewName("jsp/board/basicboardList");
-				
-				return mav;
-				 
-			}
-	
-	
-	//湲� �닔�젙而⑦듃濡�
-			@RequestMapping(value="/modifygo.html")
-			public ModelAndView modifyGo(@RequestParam(value="num") int intSeq )throws Exception{
-				
-				ModelAndView mav =new ModelAndView();
-				
-				BoardMainDto boardMainDto = boardService.getArticle(intSeq);		
-				mav.addObject("read", boardMainDto);				
-				mav.setViewName("jsp/board/basicboardModify");
-				
-				return mav; 
-			}
-	
-	//湲� �닔�젙 �븯湲�
-	@RequestMapping(value="/modify.html")
-	public ModelAndView update(BoardMainDto boardMainDto)throws Exception{
-		
-		
+
+	// 게시글 수정
+	@RequestMapping(value = "/modify.html")
+	public ModelAndView update(BoardMainDto boardMainDto) throws Exception {
+
 		boardMainDto.setStrDetailComment(boardMainDto.getStrDetailComment());
 		boardMainDto.setStrTitle(boardMainDto.getStrTitle());
-		
+
 		System.out.println(boardMainDto.getStrTitle());
-		
+
 		boardService.modifyArticle(boardMainDto);
-	
+
 		ModelAndView mav = new ModelAndView();
-		
+
 		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
 		mav.addObject("list", list);
 		mav.setViewName("jsp/board/basicboardList");
-		
+
 		return mav;
 	}
-	
-	//湲� �궘�젣�븯湲�
-	@RequestMapping(value="/delete.html")
-	public ModelAndView deleteGo(@RequestParam(value="num") int intSeq) throws Exception{
+
+	// 게시글 삭제
+	@RequestMapping(value = "/delete.html")
+	public ModelAndView deleteGo(@RequestParam(value = "num") int intSeq) throws Exception {
 		boardService.deleteArticle(intSeq);
-		
+
 		System.out.println(intSeq);
-		
+
 		ModelAndView mav = new ModelAndView();
-		
+
 		BoardMainDto boardMainDto = new BoardMainDto();
 		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
 		mav.addObject("list", list);
 		mav.setViewName("jsp/board/basicboardList");
-		
+
 		return mav;
-		 
+
 	}
-	
-	//湲� �긽�꽭蹂닿린 �뵆�윭�뒪 �뙎湲�由ъ뒪�듃,
-	@RequestMapping(value="/read.html")
-	public ModelAndView detailView(@RequestParam(value="num") int intSeq)throws Exception{
-		
-		ModelAndView mav= new ModelAndView();
-				
-		BoardMainDto boardMainDto = boardService.getArticle(intSeq);		
+
+	// 상세보기
+	@RequestMapping(value = "/read.html")
+	public ModelAndView detailView(@RequestParam(value = "num") int intSeq) throws Exception {
+
+		ModelAndView mav = new ModelAndView();
+
+		BoardMainDto boardMainDto = boardService.getArticle(intSeq);
 		mav.addObject("read", boardMainDto);
-		
+
 		ArrayList<BoardMainDto> commentList = boardService.getCommentList(intSeq);
-		mav.addObject("commentList",commentList );
-		
+		mav.addObject("commentList", commentList);
+
 		mav.setViewName("jsp/board/basicboardView");
-				
+
 		return mav;
+	}
+
+	// 댓글 입력
+	@RequestMapping(value="/addComment.html", method=RequestMethod.POST)
+	public @ResponseBody String addComment(
+			@RequestParam("ccstrDetailComment") String ccstrDetailComment, 
+			@RequestParam("ccstrWriterCode") String ccstrWriterCode, 
+			@RequestParam("ccstrGroupCode") String ccstrGroupCode, 
+			@RequestParam("ccstrTitle") String ccstrTitle,
+			@RequestParam("ccstrGroup") String ccstrGroup, 
+			@RequestParam("ccstrKindCode") String ccstrKindCode, 
+			@RequestParam("ccstrKind") String ccstrKind, 
+			@RequestParam("ccintSeq") int ccintSeq 
+			) throws Exception{
+		
+			
+		BoardMainDto boardMainDto=new BoardMainDto();
+		boardMainDto.setStrDetailComment(ccstrDetailComment);
+		boardMainDto.setStrWriterCode(ccstrWriterCode);
+		boardMainDto.setStrGroupCode(ccstrGroupCode);
+		boardMainDto.setStrGroup(ccstrGroup);
+		boardMainDto.setStrKindCode(ccstrKindCode);
+		boardMainDto.setStrKind(ccstrKind);
+		boardMainDto.setIntPseq(ccintSeq);
+		boardMainDto.setStrTitle(ccstrTitle);
+		
+		boardService.addComment(boardMainDto);
+		
+		
+		
+		JSONObject json = new JSONObject();
+		/*json.put("ccstrDetailComment", boardMainDto.getStrDetailComment());
+		json.put("ccstrWriterCode", boardMainDto.getStrName());
+		json.put("ccdatGet_Date",boardMainDto.getDatGet_Date());*/
+		
+/*		json.put("ccstrGroupCode", boardMainDto.getStrGroupCode());
+		json.put("ccstrGroup", boardMainDto.getStrGroup());
+		json.put("ccstrKindCode", value)*/
+		json.put("ok", "success");
+		return json.toJSONString();
+	}
+	
+	/*댓글쓰기 ajax 사용 안하는거
+		@RequestMapping(value="/addComment.html")
+		public ModelAndView addComment( BoardMainDto boardMainDto) throws Exception{
+			boardService.addComment(boardMainDto);
+					
+			ModelAndView mav=new ModelAndView();
+			boardMainDto = boardService.getArticle(boardMainDto.getIntSeq());
+			mav.addObject("read", boardMainDto);
+			ArrayList<BoardMainDto> commentList = boardService.getCommentList(boardMainDto.getIntSeq());
+			mav.addObject("commentList", commentList);
+			
+			mav.setViewName("jsp/board/basicboardView");
+
+			return mav;
+		}*/
+
+	
+	//댓글 수정버튼 클릭
+	@RequestMapping(value = "/modifycomment.html")
+	public @ResponseBody String modifyComment(@RequestParam("mmintSeq") int intSeq)throws Exception{
+		
+		BoardMainDto boardMainDto = boardService.getArticle(intSeq);
+		JSONObject json = new JSONObject();
+		json.put("ok", "success");
+				
+		return json.toJSONString();
 	}
 	
 	
-	
+	// 댓글 삭제
+	@RequestMapping(value = "/deletecomment.html")
+	public ModelAndView deleteComment(@RequestParam(value = "num") int intSeq, BoardMainDto boardMainDto)
+			throws Exception {
+		boardService.deleteArticle(intSeq);
+
+		System.out.println(intSeq);
+
+		ModelAndView mav = new ModelAndView();
+		ArrayList<BoardMainDto> list = boardService.getSelectBasicList(boardMainDto);
+		mav.addObject("list", list);
+		mav.setViewName("jsp/board/basicboardList");
+
+		return mav;
+
+	}
+
 }
